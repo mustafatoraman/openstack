51b4a7e49ae0768afe98472353a3bd4486080d6c
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 14:49:59 -0500

    Update install.sh

--- master/controller/install.sh
+++ master/controller/install.sh
@@ -199,3 +199,28 @@ rm -f /var/lib/neutron/neutron.sqlite
 apt-get -y install openstack-dashboard
 curl -o /etc/openstack-dashboard/local_settings.py https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/local_settings.py
 service apache2 reload
+
+#Add the Block Storage service
+mysql -uroot -p$ROOT_DB_PASS -e "CREATE DATABASE cinder"
+mysql -uroot -p$ROOT_DB_PASS -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' IDENTIFIED BY '$CINDER_DBPASS'"
+mysql -uroot -p$ROOT_DB_PASS -e "GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' IDENTIFIED BY '$CINDER_DBPASS'"
+. $rootpath/admin-openrc.sh
+openstack user create --password $CINDER_PASS cinder
+openstack role add --project service --user cinder admin
+openstack service create --name cinder --description "OpenStack Block Storage" volume
+openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
+openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
+openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
+
+apt-get -y install cinder-api cinder-scheduler python-cinderclient
+curl -o /etc/cinder/cinder.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/cinder.conf
+sh pw_update.sh /etc/cinder/cinder.conf
+su -s /bin/sh -c "cinder-manage db sync" cinder
+service nova-api restart
+service cinder-scheduler restart
+service cinder-api restart
+rm -f /var/lib/cinder/cinder.sqlite
