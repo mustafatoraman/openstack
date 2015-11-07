79c5b2bef53089ef72778ea5c4dfd1b0802e6420
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 16:21:08 -0500

    Update install.sh

--- master/controller/install.sh
+++ master/controller/install.sh
@@ -267,3 +267,35 @@ curl -o /etc/swift/swift.conf https://raw.githubusercontent.com/mustafatoraman/o
 chown -R root:swift /etc/swift
 service memcached restart
 service swift-proxy restart
+
+#Add the Orchestration service
+mysql -uroot -p$ROOT_DB_PASS -e "CREATE DATABASE heat"
+mysql -uroot -p$ROOT_DB_PASS -e "GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY '$HEAT_DBPASS'"
+mysql -uroot -p$ROOT_DB_PASS -e "GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY '$HEAT_DBPASS'"
+
+. $rootpath/admin-openrc.sh
+openstack user create --domain default --password $HEAT_PASS heat
+openstack role add --project service --user heat admin
+openstack service create --name heat --description "Orchestration" orchestration
+openstack service create --name heat-cfn --description "Orchestration"  cloudformation
+openstack endpoint create --region RegionOne orchestration public http://controller:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne orchestration internal http://controller:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne orchestration admin http://controller:8004/v1/%\(tenant_id\)s
+openstack endpoint create --region RegionOne cloudformation public http://controller:8000/v1
+openstack endpoint create --region RegionOne cloudformation internal http://controller:8000/v1
+openstack endpoint create --region RegionOne cloudformation admin http://controller:8000/v1
+openstack domain create --description "Stack projects and users" heat
+openstack user create --domain heat --password $HEAT_DOMAIN_PASS heat_domain_admin
+openstack role add --domain heat --user heat_domain_admin admin
+openstack role create heat_stack_owner
+openstack role add --project demo --user demo heat_stack_owner
+openstack role create heat_stack_user
+apt-get -y install heat-api heat-api-cfn heat-engine python-heatclient
+
+curl -o /etc/heat/heat.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/heat.conf
+sh pw_update.sh /etc/heat/heat.conf
+su -s /bin/sh -c "heat-manage db_sync" heat
+service heat-api restart
+service heat-api-cfn restart
+service heat-engine restart
+rm -f /var/lib/heat/heat.sqlite
