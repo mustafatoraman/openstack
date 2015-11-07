9e0c587c445bcda46be0cb0d1afc04b48ca9fc16
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 14:30:20 -0500

    Update install.sh

--- master/compute1/install.sh
+++ master/compute1/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+cd
 #Download Password file from controller node
 scp root@controller:/root/passwords.sh /root/passwords.sh
 rootpath=/root
 . $rootpath/passwords.sh
+
+#Download pw updater
+curl -o /root/pw_update.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/pw_update.sh
 
 #Configure NTP
 apt-get install chrony
@@ -25,3 +28,14 @@ sed -i "s/NEUTRON_PASS/$NEUTRON_PASS/g" /etc/nova/nova.conf
 curl -o /etc/nova/nova-compute.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/nova-compute.conf
 service nova-compute restart
 rm -f /var/lib/nova/nova.sqlite
+
+#Add the Networking service
+apt-get -y install neutron-plugin-linuxbridge-agent
+curl -o /etc/neutron/neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/neutron.conf
+curl -o /etc/neutron/plugins/ml2/linuxbridge_agent.ini  https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/linuxbridge_agent.ini
+
+sh pw_update.sh /etc/neutron/neutron.conf
+sh pw_update.sh /etc/neutron/plugins/ml2/linuxbridge_agent.ini 
+
+service nova-compute restart
+service neutron-plugin-linuxbridge-agent restart
