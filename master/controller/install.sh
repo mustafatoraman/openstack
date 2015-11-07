71a2468d362c390923f7afa7d561c621e8e42e34
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 03:54:43 -0500

    Update install.sh

--- master/controller/install.sh
+++ master/controller/install.sh
@@ -1,6 +1,11 @@
+#download ntp
 apt-get install chrony
-/etc/chrony/chrony.conf
+curl -o /etc/chrony/chrony.conf \
+https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/chrony.conf
+service chrony restart
+#OpenStack packages
 apt-get -y install software-properties-common
 add-apt-repository -y cloud-archive:liberty
 apt-get update && apt-get dist-upgrade
 apt-get install python-openstackclient
+#
