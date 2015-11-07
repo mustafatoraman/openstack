bdaff9f627ae231f3946ad1867ec2dc60e7845b7
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 18:19:04 -0500

    Update install-beta.sh

--- master/controller/install-beta.sh
+++ master/controller/install-beta.sh
@@ -342,7 +342,7 @@ openstack service create --name neutron --description "OpenStack Networking" net
 openstack endpoint create --region RegionOne network public http://controller:9696
 openstack endpoint create --region RegionOne network internal http://controller:9696
 openstack endpoint create --region RegionOne network admin http://controller:9696
-apt-get install neutron-server neutron-plugin-ml2 \
+apt-get -y install neutron-server neutron-plugin-ml2 \
     neutron-plugin-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent \
     neutron-metadata-agent python-neutronclient
 
