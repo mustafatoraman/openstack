2c7412f73e6362c9b8c29ac843a0f48c1e0e495d
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 12:48:31 -0500

    Create admin-openrc.sh

---
+++ master/controller/admin-openrc.sh
@@ -0,0 +1,11 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=ADMIN_PASS
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+export OS_AUTH_VERSION=3
+export OS_REGION_NAME=RegionOne
