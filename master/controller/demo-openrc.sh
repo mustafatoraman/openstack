6abb12ce9881562ebadc2937cf7503e3a7c220cc
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 12:49:10 -0500

    Create demo-openrc.sh

---
+++ master/controller/demo-openrc.sh
@@ -0,0 +1,11 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=DEMO_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+export OS_AUTH_VERSION=3
+export OS_REGION_NAME=RegionOne
