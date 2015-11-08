2f917974a82623fcccb8a9f5ed315c8598185b2f
mustafatoraman <hi@mustafatoraman.com>
2015-11-07 22:35:34 -0500

    Update install.sh

--- master/block1/install.sh
+++ master/block1/install.sh
@@ -9,7 +9,7 @@ rootpath=/root
 curl -o /root/pw_update.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/pw_update.sh
 
 #Configure NTP
-apt-get install chrony
+apt-get -y install chrony
 curl -o /etc/chrony/chrony.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/chrony.conf
 service chrony restart
 
@@ -20,7 +20,7 @@ apt-get -y update && apt-get -y dist-upgrade
 apt-get -y install python-openstackclient
 
 #Install and configure a storage node
-apt-get install lvm2
+apt-get -yinstall lvm2
 pvcreate /dev/sdb
 vgcreate cinder-volumes /dev/sdb
 curl -o /etc/lvm/lvm.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/lvm.conf
