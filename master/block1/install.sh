a8de5862dd97b3384abf796ea5aa41ec0de8614a
mustafatoraman <hi@mustafatoraman.com>
2015-11-09 18:25:59 -0500

    Update install.sh

--- master/block1/install.sh
+++ master/block1/install.sh
@@ -2,7 +2,9 @@
 cd
 
 curl -o /etc/network/interfaces https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/interfaces
+service network restart
 curl -o /etc/hostname https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hostname
+hostname block1
 curl -o /etc/hosts https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hosts
 
 #Download Password file from controller node
