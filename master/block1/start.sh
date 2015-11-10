b1d487f00426112a01ef7b63273bce6829f94c9e
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 17:12:23 -0500

    Update start.sh

--- master/block1/start.sh
+++ master/block1/start.sh
@@ -4,3 +4,4 @@ curl -o /etc/network/interfaces https://raw.githubusercontent.com/mustafatoraman
 curl -o /etc/hostname https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hostname
 curl -o /etc/hosts https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hosts
 curl -o /root/install.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/install.sh
+reboot
