e7c10fc36c9bb4676b3f2836e8bafa80e5dbfa54
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 17:10:20 -0500

    Update start.sh

--- master/controller/start.sh
+++ master/controller/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd
 
 curl -o /etc/network/interfaces https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/interfaces
 curl -o /etc/hostname https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/hostname
 curl -o /etc/hosts https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/hosts
 curl -o /root/install.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/install-beta.sh
+reboot
