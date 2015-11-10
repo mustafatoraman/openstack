116b619da75d1d552f4bc4ae31bcb49259490aea
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 17:11:57 -0500

    Update start.sh

--- master/compute1/start.sh
+++ master/compute1/start.sh
@@ -5,3 +5,4 @@ curl -o /etc/network/interfaces https://raw.githubusercontent.com/mustafatoraman
 curl -o /etc/hostname https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/hostname
 curl -o /etc/hosts https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/hosts
 curl -o /root/install.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/compute1/install.sh
+reboot
