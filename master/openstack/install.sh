c1a4f44015f383860e8079a6d90d88ad00b36c8b
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 21:14:34 -0500

    Update install.sh

--- master/openstack/install.sh
+++ master/openstack/install.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+cd
 
+clear
+read -r -p "1) Download and run password generator? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        echo "Starting..."
+curl -o /root/pw.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/pw.sh
+sh pw.sh
+rm -rf pw.sh
+        ;;
+    *)
+        echo "Moving next step..."
+        ;;
+esac
