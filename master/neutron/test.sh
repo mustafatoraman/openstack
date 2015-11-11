d228ce88d189a8145bdfdaef3bf1797c3f37a079
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 23:33:37 -0500

    Update test.sh

--- master/neutron/test.sh
+++ master/neutron/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+RUN ON OPENSTACK NODE
 
 mysql -uroot -p$ROOT_DB_PASS -e "CREATE DATABASE neutron"
 mysql -uroot -p$ROOT_DB_PASS -e "GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' IDENTIFIED BY '$NEUTRON_DBPASS'"
@@ -18,19 +19,24 @@ openstack endpoint create --region RegionOne network admin http://controller:969
 
 
 
+RUN ON NEUTRON NODE
+
+apt-get -y install apache2 libapache2-mod-wsgi memcached python-memcache
+curl -o /etc/apache2/apache2.conf https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/apache2.conf
+service apache2 restart
+
 
 apt-get -y install neutron-server neutron-plugin-ml2 \
     neutron-plugin-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent \
     neutron-metadata-agent python-neutronclient
 
-curl -o /etc/neutron/neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/neutron.conf
-curl -o /etc/neutron/plugins/ml2/ml2_conf.ini https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/ml2_conf.ini
-curl -o /etc/neutron/plugins/ml2/linuxbridge_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/linuxbridge_agent.ini
-curl -o /etc/neutron/l3_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/l3_agent.ini
-curl -o /etc/neutron/dhcp_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/dhcp_agent.ini
-curl -o /etc/neutron/dnsmasq-neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/dnsmasq-neutron.conf
-curl -o /etc/neutron/neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/neutron.conf
-curl -o /etc/neutron/metadata_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/controller/metadata_agent.ini
+curl -o /etc/neutron/neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/neutron.conf
+curl -o /etc/neutron/plugins/ml2/ml2_conf.ini https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/ml2_conf.ini
+curl -o /etc/neutron/plugins/ml2/linuxbridge_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/linuxbridge_agent.ini
+curl -o /etc/neutron/l3_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/l3_agent.ini
+curl -o /etc/neutron/dhcp_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/dhcp_agent.ini
+curl -o /etc/neutron/dnsmasq-neutron.conf https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/dnsmasq-neutron.conf
+curl -o /etc/neutron/metadata_agent.ini https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/metadata_agent.ini
 
 sh pw_update.sh /etc/neutron/neutron.conf
 sh pw_update.sh /etc/neutron/plugins/ml2/ml2_conf.ini
@@ -51,20 +57,3 @@ service neutron-metadata-agent restart
 service neutron-l3-agent restart
 
 rm -f /var/lib/neutron/neutron.sqlite
-
-
-
-rootpath=/root
-
-. $rootpath/admin-openrc.sh
-neutron net-create public --shared --provider:physical_network public --provider:network_type flat
-neutron subnet-create public 9.100.16.0/24 --name public --allocation-pool start=9.100.16.10,end=9.100.16.254 --dns-nameserver 8.8.4.4 --gateway 9.100.16.1
-. $rootpath/demo-openrc.sh
-neutron net-create private
-neutron subnet-create private 172.16.1.0/24 --name private --dns-nameserver 8.8.4.4 --gateway 172.16.1.1
-. $rootpath/admin-openrc.sh
-neutron net-update public --router:external
-. $rootpath/demo-openrc.sh
-neutron router-create router
-neutron router-interface-add router private
-neutron router-gateway-set router public
