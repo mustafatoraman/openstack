727a778f7abea1be2060bed2fb89a1467da7b2d3
mustafatoraman <hi@mustafatoraman.com>
2015-11-10 22:53:02 -0500

    Update start.sh

--- master/neutron/start.sh
+++ master/neutron/start.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+cd
 
+#Download pw updater
+curl -o /root/pw_update.sh https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/pw_update.sh
+
+#Configure NTP
+apt-get -y install chrony
+curl -o /etc/chrony/chrony.conf https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/chrony.conf
+service chrony restart
+
+#download 
+curl -o /etc/chrony/chrony.conf https://raw.githubusercontent.com/mustafatoraman/openstack/beta/master/neutron/install.sh
 
 #Install OpenStack packages
 apt-get -y install software-properties-common
