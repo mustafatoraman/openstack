e0489c586ff6a50d2e207466b790a38b2ef1073f
mustafatoraman <hi@mustafatoraman.com>
2015-11-11 20:40:40 -0500

    Update start.sh

--- master/block1/start.sh
+++ master/block1/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-curl -o /etc/network/interfaces https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/interfaces
-curl -o /etc/hostname https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hostname
-curl -o /etc/hosts https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/hosts
-curl -o /root/install.sh https://raw.githubusercontent.com/mustafatoraman/openstack/master/master/block1/install.sh
+repo=https://raw.githubusercontent.com/mustafatoraman/openstack/master/master
+
+cd
+
+curl -o /etc/network/interfaces $repo/block1/interfaces
+curl -o /etc/hostname $repo/block1/hostname
+curl -o /etc/hosts $repo/block1/hosts
+curl -o /root/install.sh $repo/block1/install.sh
+
 reboot
