60aa7b103c1f6288fb8b8a97970fd394fd9fe5ec
mustafatoraman <hi@mustafatoraman.com>
2015-11-13 15:25:23 -0500

    Update install.sh

--- master/compute1/install.sh
+++ master/compute1/install.sh
@@ -21,6 +21,8 @@ apt-get -y install software-properties-common
 add-apt-repository -y cloud-archive:liberty
 apt-get -y update && apt-get -y dist-upgrade
 apt-get -y install python-openstackclient
+apt-get install arptables
+apt-get install conntrack
 
 #Install and configure a compute node
 apt-get -y install nova-compute sysfsutils
