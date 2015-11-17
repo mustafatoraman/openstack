08ffaef8e9d420f77128504ceed62c670054de0a
mustafatoraman <hi@mustafatoraman.com>
2015-11-16 23:59:48 -0500

    Update start.sh

--- master/controller/start.sh
+++ master/controller/start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-repo=https://raw.githubusercontent.com/mustafatoraman/openstack/master/master
+. openstack.sh
 
 curl -o /etc/network/interfaces $repo/controller/interfaces
 curl -o /etc/hostname $repo/controller/hostname
 curl -o /etc/hosts $repo/controller/hosts
 curl -o /root/install.sh $repo/controller/install.sh
 
-reboot
