99406f65cbe5a3962c9f889861848b292830c25b
mustafatoraman <hi@mustafatoraman.com>
2015-11-17 01:36:29 -0500

    Update update-network.sh

--- master/update-network.sh
+++ master/update-network.sh
@@ -2,15 +2,12 @@
 rootpath=/root
 . $rootpath/config.sh
 
-echo "Downloading /etc/network/interfaces file"
-sleep1
-curl -o /etc/network/interfaces $repo/$hostname/interfaces
-clear
-echo "Downloading /etc/hostname file"
-sleep1
-curl -o /etc/hostname $repo/$hostname/hostname
-clear
-echo "Downloading /etc/hosts file"
-sleep1
-curl -o /etc/hosts $repo/$hostname/hosts
-clear
+sleep 3
+echo "Downloading /etc/network/interfaces file" >> logs
+curl -o /etc/network/interfaces $repo/$hostname/interfaces >> logs
+echo "Downloading /etc/hostname file" >> logs
+sleep 1
+curl -o /etc/hostname $repo/$hostname/hostname >> logs
+echo "Downloading /etc/hosts file" >> logs
+sleep 1
+curl -o /etc/hosts $repo/$hostname/hosts >> logs
