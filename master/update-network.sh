39977901f283f60048f8c7173eb85754f2ce4d7e
mustafatoraman <hi@mustafatoraman.com>
2015-11-17 01:44:27 -0500

    Update update-network.sh

--- master/update-network.sh
+++ master/update-network.sh
@@ -6,11 +6,14 @@ sleep 3
 clear
 echo "Downloading /etc/network/interfaces file" 
 curl -o /etc/network/interfaces $repo/$hostname/interfaces
+sleep 1
 clear
 echo "Downloading /etc/hostname file"
 sleep 1
 curl -o /etc/hostname $repo/$hostname/hostname 
+sleep 1
 clear
 echo "Downloading /etc/hosts file" 
 sleep 1
 curl -o /etc/hosts $repo/$hostname/hosts 
+sleep 1
