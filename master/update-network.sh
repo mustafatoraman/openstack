8bef078c000a19bdeb8a8be3ff4bfff8c0400c41
mustafatoraman <hi@mustafatoraman.com>
2015-11-17 01:43:52 -0500

    Update update-network.sh

--- master/update-network.sh
+++ master/update-network.sh
@@ -3,11 +3,14 @@ rootpath=/root
 . $rootpath/config.sh
 
 sleep 3
+clear
 echo "Downloading /etc/network/interfaces file" 
 curl -o /etc/network/interfaces $repo/$hostname/interfaces
+clear
 echo "Downloading /etc/hostname file"
 sleep 1
 curl -o /etc/hostname $repo/$hostname/hostname 
+clear
 echo "Downloading /etc/hosts file" 
 sleep 1
 curl -o /etc/hosts $repo/$hostname/hosts 
