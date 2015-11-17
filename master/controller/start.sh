a6c007ba2b7b04b899c2418e75fc4a61f49adaac
mustafatoraman <hi@mustafatoraman.com>
2015-11-17 00:02:54 -0500

    Update start.sh

--- master/controller/start.sh
+++ master/controller/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-. openstack.sh
+rootpath=/root
+. $rootpath/include.sh
 
 curl -o /etc/network/interfaces $repo/controller/interfaces
 curl -o /etc/hostname $repo/controller/hostname
 curl -o /etc/hosts $repo/controller/hosts
 curl -o /root/install.sh $repo/controller/install.sh
-
