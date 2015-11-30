68cb8719c5bbdd07c3d27a7405c618ade51bd7da
mustafatoraman <hi@mustafatoraman.com>
2015-11-29 21:10:59 -0500

    Update update.sh

--- master/update.sh
+++ master/update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sleep 1
 repo=https://raw.githubusercontent.com/mustafatoraman/openstack/master/master
-rm -rf /root/openstacklab.sh 
+rm -rf /root/openstacklab.sh
 wget -O /root/openstacklab.sh  $repo/openstacklab.sh  2>&1
+chmod +x /root/openstacklab.sh
 echo "Update completed."
 sleep 1
 openstacklab
